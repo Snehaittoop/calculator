6e51dcf5dbf6d054a7fe40bc8dd4a79bd421d5e0
Snehaittoop <snehaittoop868@gmail.com>
2022-09-22 19:19:18 +0530

    added

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        System.out.println("1 add 2 numbers");
+        System.out.println("2 substract 2 numbers");
+        System.out.println("3 multiply 2 numbers");
+        System.out.println("4 exit");
+        System.out.println("enter the option");
+        int option=s.nextInt();
+        switch(option){
+            case 1 :
+                System.out.println("Enter first number");
+                int a=s.nextInt();
+                System.out.println("Enter second number");
+                int b=s.nextInt();
+                System.out.println("The sum is "+(a+b));
+                break;
+            case 2 :
+                System.out.println("Enter first number");
+                int a1=s.nextInt();
+                System.out.println("Enter second number");
+                int b1=s.nextInt();
+                System.out.println("The sub is "+(a1-b1));
+                break;
+
+        }
+    }
+
+    }
